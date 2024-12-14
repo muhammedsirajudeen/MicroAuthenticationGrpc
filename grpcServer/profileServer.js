b5e067f446038ab7dcb8c0acb8398a479fbd17bc
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2024-12-14 21:40:17 +0530

    CI/CD workflow

--- grpcServer/profileServer.js
+++ grpcServer/profileServer.js
@@ -1,20 +1,19 @@
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const path = require('path');
-const AuthService=require("../service/AuthService")
+const AuthService = require("../service/AuthService")
 const protoPath = path.join(__dirname, '../proto/profile.proto');
 const packageDefinition = protoLoader.loadSync(protoPath, {});
 const userProto = grpc.loadPackageDefinition(packageDefinition).user;
 
 async function addProfile(call, callback) {
   console.log(call.request)
-  const {username,password}=call.request
-  const res=await AuthService.SignUpUser('profiles',username,password)
-  if(res){
-    callback(null, {message:'success'});
-  }else{
-    callback(null, {message:'failed to create profile'});
-
+  const { username, password } = call.request
+  const res = await AuthService.SignUpUser('profiles', username, password)
+  if (res) {
+    callback(null, { message: 'success' });
+  } else {
+    callback(null, { message: 'failed to create profile' });
   }
 
 }
